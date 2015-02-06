571dd96703ca3c60bf7c14016a05769167768c94
baljeetSingh21 <baljeet.singh@venturepact.com>
2015-02-06 10:55:53 +0530

    Initial Commit

---
+++ demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by Vicky Mathneja on 06/02/15.
+//  Copyright (c) 2015 LPU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
