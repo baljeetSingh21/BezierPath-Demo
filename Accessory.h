bd45d3c8c1073e2f90e3d75ff9895318a5f3a7dd
baljeetSingh21 <baljeet.singh@venturepact.com>
2015-02-14 12:40:09 +0530

    first commit

---
+++ Accessory.h
@@ -0,0 +1,13 @@
+//
+//  Accessory.h
+//  demo
+//
+//  Created by Vicky Mathneja on 06/02/15.
+//  Copyright (c) 2015 LPU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Accessory : UIView
+
+@end
---
+++ Accessory.m
@@ -0,0 +1,21 @@
+//
+//  Accessory.m
+//  demo
+//
+//  Created by Vicky Mathneja on 06/02/15.
+//  Copyright (c) 2015 LPU. All rights reserved.
+//
+
+#import "Accessory.h"
+
+@implementation Accessory
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
